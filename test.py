1fb66ded8b4060884f27f97e6945e2434b3ad8e6
wideindeed <wideindeed@gmail.com>
2026-06-17 04:25:45 +0400

    Cleaned up file locations into proper folders

--- test.py
+++ test.py
@@ -1,9 +1,14 @@
 import os
+import sys
 
 # Must be set before any AI/GPU library is imported
 os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
 os.environ['CUDA_VISIBLE_DEVICES']   = '-1'
 os.environ["QT_AUTO_SCREEN_SCALE_FACTOR"] = "1"
+
+
+# PhysioVision: make src/ importable from any working directory
+sys.path.insert(0, os.path.join(os.path.dirname(os.path.abspath(__file__)), 'src'))
 
 from dashboard import run_application
 
